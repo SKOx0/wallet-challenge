3973fc98635cae563eced43e82ff7864e1f51bee
Victor Mesquita <skovhm@gmail.com>
2018-06-05 23:08:01 -0300

    Adicionado root reducers

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,24 +1,12 @@
 import { combineReducers } from 'redux';
-import { LOCATION_CHANGE } from 'react-router-redux';
-import appReducer from '../components/container/App/reducer';
+import homeReducer from 'components/container/Home/reducer';
+
+import routeReducer from './location';
+
+
 // Modulo responsável por combinar todos os reducers
 
-const routeInitialState = {
-  location: null,
-};
-
-function routeReducer(state = routeInitialState, action) {
-  switch (action.type) {
-    case LOCATION_CHANGE:
-      return { ...state, location: action.payload };
-    default:
-      return state;
-  }
-}
-
-export default function createReducer() {
-  return combineReducers({
-    route: routeReducer,
-    global: appReducer,
-  });
-}
+export default combineReducers({
+  route: routeReducer,
+  homeReducer
+});
