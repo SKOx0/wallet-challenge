778ab099aa275bb2dfd9c3838a155b49ce0c751d
Victor Mesquita <victormesquita@brq.com>
2018-06-13 15:04:33 -0300

    Melhoria lógica consulta ao banco do brasil
    Em caso de indisponibilidade de taxas, a consulta é feita com data inicial do primeiro dia do mês em questão

--- server/plugins/graphql/resolvers/brita.js
+++ server/plugins/graphql/resolvers/brita.js
@@ -1,19 +1,30 @@
 const axios = require('axios');
 const dateformat = require('dateformat');
 const env = require('../../../env/env');
-const { round } = require('../../../helpers/decimal');
+const {
+  round
+} = require('../../../helpers/decimal');
 
-const getBritaPrice = async () => {
-  const today = new Date();
-  const yesterday = new Date().setDate(today.getDate() - 1);
+function getUrl(dataInicial, dataFinal) {
+  return `${env.bitraApi}?@moeda='USD'&@dataInicial='${dateformat(dataInicial, 'dd-mm-yyyy')}'&@dataFinalCotacao='${dateformat(dataFinal, 'dd-mm-yyyy')}'&$format=json&$orderby=dataHoraCotacao desc&$top=1`;
+}
 
-  const result = await axios.get(`${env.bitraApi}?@moeda='USD'&@dataInicial='${dateformat(yesterday, 'dd-mm-yyyy')}'&@dataFinalCotacao='${dateformat(today, 'dd-mm-yyyy')}'&$format=json&$orderby=dataHoraCotacao desc&$top=1`);
+const requestBrita = async (dataInicial, dataFinal) => {
+  const model = {
+    buy: null,
+    sell: null
+  };
 
-  const model = { buy: null, sell: null };
+  const result = await axios.get(getUrl(dataInicial, dataFinal));
 
-  if (result.status !== 200) { return model; }
+  if (result.status !== 200) {
+    return model;
+  }
 
-  const { cotacaoCompra, cotacaoVenda } = result.data.value[0];
+  const {
+    cotacaoCompra,
+    cotacaoVenda
+  } = result.data.value[0];
 
   model.buy = round(cotacaoCompra, 4);
   model.sell = round(cotacaoVenda, 4);
@@ -21,4 +32,29 @@ const getBritaPrice = async () => {
   return model;
 };
 
-module.exports = { getBritaPrice };
+const getBritaPrice = async () => {
+  let dataFinal = new Date();
+  let dataInicial = new Date().setDate(dataFinal.getDate() - 1);
+
+  try {
+    const model = await requestBrita(dataInicial, dataFinal);
+
+    return model;
+  } catch (ex) {
+    // Em caso de falha para obter as taxas de hoje, faz uma consulta com o primeiro dia do mês
+    const today = new Date();
+
+    dataFinal = dataFinal.setDate(today.getDate() - 1);
+
+    dataInicial = new Date(today.getFullYear(), today.getMonth(), 1);
+
+    const model = await requestBrita(dataInicial, dataFinal);
+
+    return model;
+  }
+};
+
+
+module.exports = {
+  getBritaPrice
+};
