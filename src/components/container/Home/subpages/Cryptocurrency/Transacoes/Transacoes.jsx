5c2a8facf584ea21c5554618b34b598c11627cba
Victor Mesquita <skovhm@gmail.com>
2018-06-15 02:33:15 -0300

    Transações

--- src/components/container/Home/subpages/Cryptocurrency/Transacoes/Transacoes.jsx
+++ src/components/container/Home/subpages/Cryptocurrency/Transacoes/Transacoes.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
-import { Content } from 'bloomer';
+import { Content, Table } from 'bloomer';
 
 const Transacoes = () => (
   <Content>
-  Transacões
+    <Table >
+      <thead>
+        <tr>
+          <th>Data</th>
+          <th>Tipo transação</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>15/06/2018</td>
+          <td>Venda</td>
+        </tr>
+        <tr>
+          <td>15/06/2018</td>
+          <td>Troca</td>
+        </tr>
+        <tr>
+          <td>15/06/2018</td>
+          <td>Compra</td>
+        </tr>
+      </tbody>
+    </Table>
   </Content>
 );
 
